329c83743d3499be9d118ac01452528472ebdb52
Anton Mykhailenko <mykhailencko2594@gmail.com>
2023-03-13 01:11:41 +0200

    added effect for riffle and projectile

--- Source/ShootThemUp/Private/Weapon/Components/STUWeaponFXComponent.cpp
+++ Source/ShootThemUp/Private/Weapon/Components/STUWeaponFXComponent.cpp
@@ -3,6 +3,7 @@
 
 #include "Weapon/Components/STUWeaponFXComponent.h"
 #include "NiagaraFunctionLibrary.h"
+#include "PhysicalMaterials/PhysicalMaterial.h"
 
 // Sets default values for this component's properties
 USTUWeaponFXComponent::USTUWeaponFXComponent()
@@ -16,5 +17,16 @@ USTUWeaponFXComponent::USTUWeaponFXComponent()
 
 void USTUWeaponFXComponent::PlayImpactFX(const FHitResult& Hit) 
 {
+    auto Effect = DefaultEffect;
+
+	if (Hit.PhysMaterial.IsValid())
+    {
+        const auto PlysMat = Hit.PhysMaterial.Get();
+        if (EffectsMap.Contains(PlysMat))
+        {
+            Effect = EffectsMap[PlysMat];
+        }
+	}
+
     UNiagaraFunctionLibrary::SpawnSystemAtLocation(GetWorld(), Effect, Hit.ImpactPoint, Hit.ImpactNormal.Rotation());
 }
